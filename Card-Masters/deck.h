d83be10174ebc3045c8b6dbd893fbbfce8a4d6dd
nikoldimitrova <nikol___97@abv.bg>
2017-04-05 12:00:13 +0300

    Update deck.h

--- Card-Masters/deck.h
+++ Card-Masters/deck.h
@@ -1,6 +1,16 @@
 #ifndef DECK_H_INCLUDED
 #define DECK_H_INCLUDED
+class Deck {
+private:
+	int* Hand;
+	int* Cards;
 
-
-
+public:
+	Deck();
+  ~Deck();
+	void AddCard(int);
+	void GetCard(int);
+	void RemoveCard(int);
+	void PrintHand();
+};
 #endif // DECK_H_INCLUDED
